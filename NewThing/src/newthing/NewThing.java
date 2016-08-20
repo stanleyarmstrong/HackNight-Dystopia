24119993d9d16686a6f533191af9757d2effd06f
stonewallstan <stanleyarmstrong31@gmail.com>
2016-08-19 20:59:33 -0700

    Stuff for HackNight Dystopia

---
+++ NewThing/src/newthing/NewThing.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newthing;
+
+/**
+ *
+ * @author Stanley
+ */
+public class NewThing {
+    public NewThing(){
+        String person = "Jason Bourne";
+        System.out.println("Hello I am the savior " + person + ". I was Bourne out of a meme.");
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("The world has flipped upside down. \n The world needed a hero. \n Someone they could trust that man was... \n Jason Bourne ");
+        NewThing jason = new NewThing(); // TODO code application logic here
+    }
+    
+}
---
+++ NewThing/src/newthing/Sword.java
@@ -0,0 +1,19 @@
+package newthing; 
+
+
+public class Sword{
+    public Sword(){
+        int swing = -10 ; 
+        int superSwing = -20 ; 
+        int suicide = 100 ; 
+          
+    }
+    void ifFound(){
+        boolean found = true ; 
+        if(found){
+            System.out.println("You have found sword 1 of many swords. This one is very weak to upgrade. If you want to upgrade this you can either upgrade in the menu bar or find a not dead blacksmith.");
+            System.out.println("Happy hunting! heh heh heh heh heh ");
+        }
+    }
+}
+

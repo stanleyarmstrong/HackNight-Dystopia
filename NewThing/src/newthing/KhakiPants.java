02229fc6d4519fe85a54020dedd58ea3b5328f3d
stonewallstan <stanleyarmstrong31@gmail.com>
2016-08-21 14:53:21 -0700

    Made Some Edits
    Added characters that sell Khaki Pants. Started working on characters
    who sell things.The old tennis man will sell tennis themed things and he
    will also sell Khaki pants.

--- NewThing/src/newthing/KhakiPants.java
+++ NewThing/src/newthing/KhakiPants.java
@@ -6,6 +6,10 @@ public class KhakiPants{
         String typeOfArmor = "Leg Armor" ; 
         int armorRating = 15 ;
         HashMap<String,Integer> cost = new HashMap<String,Integer>();
+        cost.put("Old Tennis Man", 25);
+        cost.put("The One After Two", 16);
+        cost.put("The Founder", 20);
+        cost.put("The man with Blankets", 35);
         
         
     }
---
+++ NewThing/src/newthing/OldTennisMan.java
@@ -0,0 +1,15 @@
+package newthing;
+
+import java.util.*;
+
+public class OldTennisMan{
+    public OldTennisMan(){
+        Scanner firstQuestion = new Scanner(System.in);
+        System.out.println("Hello, you have met me, the Old Tennis Man.I know who you are already, Jason Bourne.What would you like to talk about? \n -Buying something\n -Selling Something \n -About the World  ");
+        String answer = firstQuestion.next();
+        switch(answer){
+            case "":
+        }
+        
+    }
+}
